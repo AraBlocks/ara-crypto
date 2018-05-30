ab5d54d47b84382253bc562665ad0cfee4a52115
Joseph Werle <joseph.werle@gmail.com>
2018-05-30 15:50:58 -0400

    feat(base64.js): Introduce simple base64 wrapper

---
+++ base64.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const base64 = require('base-x')(
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+)
+
+function encode(value) {
+  return base64.encode(value)
+}
+
+function decode(value) {
+  return base64.decode(value)
+}
+
+module.exports = {
+  encode,
+  decode,
+}
