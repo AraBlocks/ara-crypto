fb2750af0e867bfe694e329497b89227b027f1a0
Joseph Werle <joseph.werle@gmail.com>
2018-06-19 17:04:08 -0400

    refactor(index.js): Export curve25519 module in index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { blake2b } = require('./blake2b')
 const { decrypt } = require('./decrypt')
 const { encrypt } = require('./encrypt')
 const { keyPair } = require('./key-pair')
+const curve25519 = require('./curve25519')
 const { verify } = require('./verify')
 const { sign } = require('./sign')
 const base58 = require('./base58')
@@ -15,6 +16,7 @@ const uint64 = require('./uint64')
 module.exports = {
   discoveryKey,
   randomBytes,
+  curve25519,
   blake2b,
   decrypt,
   encrypt,
