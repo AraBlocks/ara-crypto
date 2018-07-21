a6d8d095c3000cd49b01f08f36a5d80314f282f6
Joseph Werle <joseph.werle@gmail.com>
2018-07-21 10:07:03 -0400

    refactor(random-bytes.js): Rework type check logic for better coverage

--- random-bytes.js
+++ random-bytes.js
@@ -12,10 +12,10 @@ const alloc = require('buffer-alloc-unsafe')
  * @throws TypeError
  */
 function randomBytes(size) {
-  if (!size || 'number' !== typeof size) {
-    throw new TypeError('crypto.randomBytes: Expecting size to be a number.')
-  } else if (Number.isNaN(size)) {
+  if (Number.isNaN(size)) {
     throw new TypeError('crypto.randomBytes: Size cannot be NaN.')
+  } else if (!size || 'number' !== typeof size) {
+    throw new TypeError('crypto.randomBytes: Expecting size to be a number.')
   } else if (size <= 0) {
     throw new TypeError('crypto.randomBytes: Size must be larger than 0.')
   }
