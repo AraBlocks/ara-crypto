01b408598deb4d81db3afac3675b42074574ba43
Joseph Werle <joseph.werle@gmail.com>
2018-07-23 15:10:46 -0400

    test(test/keyPair.js): Simplify keyPair test

--- test/key-pair.js
+++ test/key-pair.js
@@ -1,17 +1,8 @@
 const { keyPair } = require('../key-pair')
-const isBuffer = require('is-buffer')
+const ed25519 = require('../ed25519')
 const test = require('ava')
 
-test('keyPair(seed)', async (t) => {
-  t.throws(() => keyPair(null), TypeError)
-  t.throws(() => keyPair(0), TypeError)
-  t.throws(() => keyPair(''), TypeError)
-  t.throws(() => keyPair(true), TypeError)
-  t.throws(() => keyPair([]), TypeError)
-  t.true('object' === typeof keyPair())
-  t.true('object' === typeof keyPair(Buffer.alloc(32).fill('hello')))
-  t.true(isBuffer(keyPair().publicKey))
-  t.true(isBuffer(keyPair().secretKey))
-  t.true(32 === keyPair().publicKey.length)
-  t.true(64 === keyPair().secretKey.length)
+test('keyPair()', async (t) => {
+  t.true('function' === typeof keyPair)
+  t.true(keyPair === ed25519.keyPair)
 })
