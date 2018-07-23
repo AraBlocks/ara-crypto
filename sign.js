990deae682bb46aff927346c727b0f7a5938f2dc
Joseph Werle <joseph.werle@gmail.com>
2018-07-23 15:08:29 -0400

    refactor(sign.js): Use ed25519.sign for default sign

--- sign.js
+++ sign.js
@@ -1,39 +1,4 @@
-const isBuffer = require('is-buffer')
-const alloc = require('buffer-alloc-unsafe')
-
-/* eslint-disable camelcase */
-const {
-  crypto_sign_detached,
-  crypto_sign_BYTES,
-} = require('sodium-universal')
-
-/**
- * Sign a message buffer with a secret key buffer. This function calls
- * `crypto_sign_detached` on a buffer of size `crypto_sign_BYTES`.
- *
- * @public
- * @param {Buffer} message
- * @param {Buffer} secretKey
- * @return {Buffer}
- * @throws TypeError
- */
-function sign(message, secretKey) {
-  if (!message || false === isBuffer(message)) {
-    throw new TypeError('crypto.sign: Expecting message to be a buffer.')
-  } else if (0 === message.length) {
-    throw new TypeError('crypto.sign: Cannot sign an empty message.')
-  }
-
-  if (!secretKey || false === isBuffer(secretKey)) {
-    throw new TypeError('crypto.sign: Expecting secretKey to be a buffer.')
-  } else if (0 === secretKey.length) {
-    throw new TypeError('crypto.sign: Cannot sign with an empty secretKey.')
-  }
-
-  const buffer = alloc(crypto_sign_BYTES)
-  crypto_sign_detached(buffer, message, secretKey)
-  return buffer
-}
+const { sign } = require('./ed25519')
 
 module.exports = {
   sign
