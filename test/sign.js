b98f0e801b78eb55b7e346589a0c4db973c37bbd
Joseph Werle <joseph.werle@gmail.com>
2018-07-23 15:09:50 -0400

    test(test/sign.js): Simplify sign test

--- test/sign.js
+++ test/sign.js
@@ -1,24 +1,8 @@
-const { keyPair } = require('../key-pair')
 const { sign } = require('../sign')
-const isBuffer = require('is-buffer')
+const ed25519 = require('../ed25519')
 const test = require('ava')
 
-test('sign(message, secretKey)', async (t) => {
-  t.throws(() => sign(0, 0), TypeError)
-  t.throws(() => sign(null, 0), TypeError)
-  t.throws(() => sign(true, 0), TypeError)
-  t.throws(() => sign([], 0), TypeError)
-  t.throws(() => sign('', 0), TypeError)
-  t.throws(() => sign(Buffer.from(0), 0), TypeError)
-
-  t.throws(() => sign(Buffer.from('message'), 0), TypeError)
-  t.throws(() => sign(Buffer.from('message'), null), TypeError)
-  t.throws(() => sign(Buffer.from('message'), true), TypeError)
-  t.throws(() => sign(Buffer.from('message'), []), TypeError)
-  t.throws(() => sign(Buffer.from('message'), ''), TypeError)
-  t.throws(() => sign(Buffer.from('message'), Buffer.from(0)), TypeError)
-
-  const { secretKey } = keyPair()
-
-  t.true(isBuffer(sign(Buffer.from('message'), secretKey)))
+test('sign()', async (t) => {
+  t.true('function' === typeof sign)
+  t.true(sign === ed25519.sign)
 })
