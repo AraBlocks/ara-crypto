e06c28624f657563ce17d20f05483648ecd01ea9
Joseph Werle <joseph.werle@gmail.com>
2018-07-25 15:42:40 -0400

    refactor(constants.js): Remove useless constants

--- constants.js
+++ constants.js
@@ -11,28 +11,13 @@ const [
 /* eslint-disable no-mixed-operators */
 const kVersion = versions[0] << 16 | versions[1] << 8 | versions[2]
 
-// blake2b()
-const kDefaultBlake2bSize = 32
-
 // encryption/decryption
 const kDefaultCipher = 'aes-128-ctr'
 const kDefaultDigest = 'sha1'
 
-// discoveryKey
-const kDefaultDiscoveryKeySize = 32
-
-// uint64
-const kMinUInt64Size = 8
-const kDefaultUInt64Size = kMinUInt64Size
-
 module.exports = {
-  kDefaultDiscoveryKeySize,
-  kDefaultBlake2bSize,
-  kDefaultUInt64Size,
   kDefaultCipher,
   kDefaultDigest,
-
-  kMinUInt64Size,
 
   kVersionMajor,
   kVersionMinor,
